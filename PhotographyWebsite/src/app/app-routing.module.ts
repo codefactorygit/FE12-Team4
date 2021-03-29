cb2ae8f1c86d6026d8ca4458e600429793fadb5d
Freddy <freddy.delgado.porras@gmail.com>
2021-03-29 15:48:43 +0200

    new routing

--- PhotographyWebsite/src/app/app-routing.module.ts
+++ PhotographyWebsite/src/app/app-routing.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+import { BioAboutComponent } from './bio-about/bio-about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: "portfolio", component:PortfolioComponent},
+  {path: "bio-about", component:BioAboutComponent},
   {
     path: '', component: HomeComponent
   }
-=======
-import { BioAboutComponent } from './bio-about/bio-about.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-
-const routes: Routes = [
-  {path:"portfolio", component:PortfolioComponent},
-  {path:"bio-about", component:BioAboutComponent},
->>>>>>> a9b1843e9e349dadc24789483b7df3a7624786ed
 ];
 
 @NgModule({
