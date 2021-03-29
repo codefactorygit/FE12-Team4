8296c8f3ce68e47d3d70a211fcd455c7b4edcbe5
Natalia <natalia_bochenek@hotmail.com>
2021-03-29 13:44:57 +0200

    adding paths

--- PhotographyWebsite/src/app/app-routing.module.ts
+++ PhotographyWebsite/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CustomersComponent } from './customers/customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contact-us', component: ContactUsComponent
+  },
+  {
+    path: 'customers', component: CustomersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
