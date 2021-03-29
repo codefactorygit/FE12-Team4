bba62cc63e2cd761bcd6b977fcf35ce9d8e012cb
Marko Grahovac <marko.grahovac7@gmail.com>
2021-03-29 14:15:48 +0200

    Routing Paths added

--- PhotographyWebsite/src/app/app-routing.module.ts
+++ PhotographyWebsite/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BioAboutComponent } from './bio-about/bio-about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"portfolio", component:PortfolioComponent},
+  {path:"bio-about", component:BioAboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
