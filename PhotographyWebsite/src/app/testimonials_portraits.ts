a119afe47c671f3e69819954912461edab308903
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-30 22:48:14 +0200

    add testimonial_portraits.ts + *ngFor loop cards

---
+++ PhotographyWebsite/src/app/testimonials_portraits.ts
@@ -0,0 +1,67 @@
+export const portraits = [
+  {
+    columns: '4',
+    image: 'assets/images/testimonials/portrait_01.jpg',
+    review: 'Thanks for the shooting!',
+  },
+  {
+    columns: '2',
+    image: 'assets/images/testimonials/portrait_02.jpg',
+    review: 'Superb!',
+  },
+  {
+    columns: '4',
+    image: 'assets/images/testimonials/portrait_03.jpg',
+    review: 'Really gorgous!',
+  },
+  {
+    columns: '2',
+    image: 'assets/images/testimonials/portrait_04.jpg',
+    review: 'Just wow!',
+  },
+  {
+    columns: '2',
+    image: 'assets/images/testimonials/portrait_05.jpg',
+    review: 'Top notch!',
+  },
+  {
+    columns: '4',
+    image: 'assets/images/testimonials/portrait_06.jpg',
+    review: 'Awesome work!',
+  },
+  {
+    columns: '2',
+    image: 'assets/images/testimonials/portrait_07.jpg',
+    review: 'Great!',
+  },
+  {
+    columns: '4',
+    image: 'assets/images/testimonials/portrait_08.jpg',
+    review: 'So cute!',
+  },
+  {
+    columns: '2',
+    image: 'assets/images/testimonials/portrait_09.jpg',
+    review: 'Thank you!',
+  },
+  {
+    columns: '2',
+    image: 'assets/images/testimonials/portrait_10.jpg',
+    review: 'Phantastic!',
+  },
+  {
+    columns: '2',
+    image: 'assets/images/testimonials/portrait_11.jpg',
+    review: 'Perfect!',
+  },
+  {
+    columns: '4',
+    image: 'assets/images/testimonials/portrait_12.jpg',
+    review: 'So authentic!',
+  },
+  {
+    columns: '2',
+    image: 'assets/images/testimonials/portrait_13.jpg',
+    review: 'Thanks!',
+  },
+];
